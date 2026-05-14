6bfccd3b108a577f28f9b519e99898dd2e6eb032
MD AHSAN HABIB <MD AHSAN HABIB>
2026-05-14 22:01:05 +0600

    Solve data fetching Error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL || "https://globtrek-backend.vercel.app",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
 });
 
